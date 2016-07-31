5b0d11abf733ad124bc5e87ffc04b4e3677ba6de
Japheth Balane <japheth162@gmail.com>
2016-07-31 13:24:46 +0800

    . . .

--- script.js
+++ script.js
@@ -20,7 +20,11 @@ var numbers = [
 	[1,1,1,0,0,1,1]  	// nine
 ];
 
+var letters = ['A','B','C','D','E','F','G','H','I','J','K',
+	'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 var clock = new Clock();
+var alphaclock = new AlphaClock();
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -47,6 +51,8 @@ function world() {
 	var seconds = now.getSeconds();
 
 	clock.update(hours,minutes,seconds).draw();
+	alphaclock.update(hours,minutes,seconds).draw();
+
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -63,6 +69,33 @@ function toBinary(num) {
 
 //////////////////////////////////////////////////////////////////////////////
 
+function AlphaClock() {
+	this.x = clock.x;
+	this.y = clock.y-30;
+
+	this.arr = [];
+
+	for (var i = 0; i < 38; i++) {
+		this.arr.push(letters[randomBetween(0,25)]);
+	}
+
+	var gap = 15;
+
+	this.update = function(h,m,s) {
+		var rand = randomBetween(0,(this.arr).length);
+		(this.arr)[rand] = letters[randomBetween(0,25)];
+		return this;
+	}
+
+	this.draw = function() {	
+		context.font = "10px Arial";
+		context.fillStyle = "#f00";
+		for (var i = 0; i < (this.arr).length; i++) {
+			context.fillText((this.arr)[i],this.x+gap*i,this.y);
+		}
+	}
+}
+
 function Clock() {
 	this.digitHeight = 100;
 	this.x = canvas.width/2 - (this.digitHeight*6)/2;
@@ -75,32 +108,32 @@ function Clock() {
 	this.s1 = new Cell(this.x+400,this.y,100);
 	this.s2 = new Cell(this.x+500,this.y,100);
 
-	this.h1_bin = new Cell(this.x+19.8*0,this.y+110,25);
-	this.h2_bin = new Cell(this.x+19.8*1,this.y+110,25);
-	this.h3_bin = new Cell(this.x+19.8*2,this.y+110,25);
-	this.h4_bin = new Cell(this.x+19.8*3,this.y+110,25);
-	this.h5_bin = new Cell(this.x+19.8*5,this.y+110,25);
-	this.h6_bin = new Cell(this.x+19.8*6,this.y+110,25);
-	this.h7_bin = new Cell(this.x+19.8*7,this.y+110,25);
-	this.h8_bin = new Cell(this.x+19.8*8,this.y+110,25);
+	this.h1_bin = new Cell(this.x+19.8*0,this.y+130,15);
+	this.h2_bin = new Cell(this.x+19.8*1,this.y+130,15);
+	this.h3_bin = new Cell(this.x+19.8*2,this.y+130,15);
+	this.h4_bin = new Cell(this.x+19.8*3,this.y+130,15);
+	this.h5_bin = new Cell(this.x+19.8*5,this.y+130,15);
+	this.h6_bin = new Cell(this.x+19.8*6,this.y+130,15);
+	this.h7_bin = new Cell(this.x+19.8*7,this.y+130,15);
+	this.h8_bin = new Cell(this.x+19.8*8,this.y+130,15);
 
-	this.m1_bin = new Cell(this.x+19.8*10,this.y+110,25);
-	this.m2_bin = new Cell(this.x+19.8*11,this.y+110,25);
-	this.m3_bin = new Cell(this.x+19.8*12,this.y+110,25);
-	this.m4_bin = new Cell(this.x+19.8*13,this.y+110,25);
-	this.m5_bin = new Cell(this.x+19.8*15,this.y+110,25);
-	this.m6_bin = new Cell(this.x+19.8*16,this.y+110,25);
-	this.m7_bin = new Cell(this.x+19.8*17,this.y+110,25);
-	this.m8_bin = new Cell(this.x+19.8*18,this.y+110,25);
+	this.m1_bin = new Cell(this.x+19.8*10,this.y+130,15);
+	this.m2_bin = new Cell(this.x+19.8*11,this.y+130,15);
+	this.m3_bin = new Cell(this.x+19.8*12,this.y+130,15);
+	this.m4_bin = new Cell(this.x+19.8*13,this.y+130,15);
+	this.m5_bin = new Cell(this.x+19.8*15,this.y+130,15);
+	this.m6_bin = new Cell(this.x+19.8*16,this.y+130,15);
+	this.m7_bin = new Cell(this.x+19.8*17,this.y+130,15);
+	this.m8_bin = new Cell(this.x+19.8*18,this.y+130,15);
 
-	this.s1_bin = new Cell(this.x+19.8*20,this.y+110,25);
-	this.s2_bin = new Cell(this.x+19.8*21,this.y+110,25);
-	this.s3_bin = new Cell(this.x+19.8*22,this.y+110,25);
-	this.s4_bin = new Cell(this.x+19.8*23,this.y+110,25);
-	this.s5_bin = new Cell(this.x+19.8*25,this.y+110,25);
-	this.s6_bin = new Cell(this.x+19.8*26,this.y+110,25);
-	this.s7_bin = new Cell(this.x+19.8*27,this.y+110,25);
-	this.s8_bin = new Cell(this.x+19.8*28,this.y+110,25);
+	this.s1_bin = new Cell(this.x+19.8*20,this.y+130,15);
+	this.s2_bin = new Cell(this.x+19.8*21,this.y+130,15);
+	this.s3_bin = new Cell(this.x+19.8*22,this.y+130,15);
+	this.s4_bin = new Cell(this.x+19.8*23,this.y+130,15);
+	this.s5_bin = new Cell(this.x+19.8*25,this.y+130,15);
+	this.s6_bin = new Cell(this.x+19.8*26,this.y+130,15);
+	this.s7_bin = new Cell(this.x+19.8*27,this.y+130,15);
+	this.s8_bin = new Cell(this.x+19.8*28,this.y+130,15);
 
 	this.update = function(h,m,s) {
 		(this.h1).currentNumber = Math.floor(h/10)%10;
@@ -110,17 +143,56 @@ function Clock() {
 		(this.s1).currentNumber = Math.floor(s/10)%10;
 		(this.s2).currentNumber = s%10;
 
-		// (this.h1_bin).currentNumber = 
-		// 	Math.floor(parseInt(toBinary(Math.floor(h/10)%10))/1000);
-		// (this.h2_bin).currentNumber = 
-		// 	Math.floor(parseInt(toBinary(Math.floor(h/10)%10))/100);
-		// (this.h3_bin).currentNumber = 
-		// 	Math.floor(parseInt(toBinary(Math.floor(h/10)%10))/10);
-		// (this.h4_bin).currentNumber = 
-		// 	Math.floor(parseInt(toBinary(Math.floor(h/10)%10))/1);
+		(this.h1_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(h/10)%10)/1000);
+		(this.h2_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(h/10)%10)/100)%10;
+		(this.h3_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(h/10)%10)/10)%10;
+		(this.h4_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(h/10)%10)/1)%10;
+		(this.h5_bin).currentNumber =
+			Math.floor(toBinary(h%10)/1000);
+		(this.h6_bin).currentNumber =
+			Math.floor(toBinary(h%10)/100)%10;
+		(this.h7_bin).currentNumber =
+			Math.floor(toBinary(h%10)/10)%10;
+		(this.h8_bin).currentNumber =
+			Math.floor(toBinary(h%10)/1)%10;
 
-		// (this.h5_bin).currentNumber = Math.floor(parseInt(toBinary(h%10))/1000);
-		// (this.h6_bin).currentNumber = Math.floor;
+		(this.m1_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(m/10)%10)/1000);
+		(this.m2_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(m/10)%10)/100)%10;
+		(this.m3_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(m/10)%10)/10)%10;
+		(this.m4_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(m/10)%10)/1)%10;
+		(this.m5_bin).currentNumber =
+			Math.floor(toBinary(m%10)/1000);
+		(this.m6_bin).currentNumber =
+			Math.floor(toBinary(m%10)/100)%10;
+		(this.m7_bin).currentNumber =
+			Math.floor(toBinary(m%10)/10)%10;
+		(this.m8_bin).currentNumber =
+			Math.floor(toBinary(m%10)/1)%10;		
+
+		(this.s1_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(s/10)%10)/1000);
+		(this.s2_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(s/10)%10)/100)%10;
+		(this.s3_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(s/10)%10)/10)%10;
+		(this.s4_bin).currentNumber =
+			Math.floor(toBinary(Math.floor(s/10)%10)/1)%10;
+		(this.s5_bin).currentNumber =
+			Math.floor(toBinary(s%10)/1000);
+		(this.s6_bin).currentNumber =
+			Math.floor(toBinary(s%10)/100)%10;
+		(this.s7_bin).currentNumber =
+			Math.floor(toBinary(s%10)/10)%10;
+		(this.s8_bin).currentNumber =
+			Math.floor(toBinary(s%10)/1)%10;
 
 
 		return this;
